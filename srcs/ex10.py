9961cac96343cdde13afbc51d3d721e267b0310c
Sungyong Cho <here@sungyongcho.com>
2023-07-03 04:58:47 +0200

    fix: ex10 eval done

--- srcs/ex10.py
+++ srcs/ex10.py
@@ -25,3 +25,21 @@ u = Matrix([[8., 5., -2., 4., 28.],
 # [0.0, 0.0, 1.0, 0.0, -3.6666667]
 # [0.0, 0.0, 0.0, 1.0, 29.5 ]
 print(u.row_echelon())
+
+print("=======eval===========")
+u = Matrix([[0., 0.], [0., 0.]])
+print(u.row_echelon())
+
+u = Matrix([[1., 0.], [0., 1.]])
+print(u.row_echelon())
+
+u = Matrix([[4., 2.], [2., 1.]])
+print(u.row_echelon())
+
+u = Matrix([[-7., 2.], [4., 8.]])
+print(u.row_echelon())
+
+u = Matrix([[1., 2.], [4., 8.]])
+print(u.row_echelon())
+
+print("=======eval===========")
