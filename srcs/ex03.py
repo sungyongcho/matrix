3bdbd5e9126685b4c0e77da3eb9b71b264e1beef
Sungyong Cho <here@sungyongcho.com>
2023-07-03 04:41:09 +0200

    fix: ex03 eval done

--- srcs/ex03.py
+++ srcs/ex03.py
@@ -27,3 +27,31 @@ print(u.shape, v.shape)
 
 # 9.0
 print(u.dot(v))
+
+print("=======eval===========")
+
+u = Vector([[0.], [0.]])
+v = Vector([[0.], [0.]])
+print(u.dot(v))
+
+u = Vector([[1.], [0.]])
+v = Vector([[0.], [0.]])
+print(u.dot(v))
+
+u = Vector([[1.], [0.]])
+v = Vector([[1.], [0.]])
+print(u.dot(v))
+
+u = Vector([[1.], [0.]])
+v = Vector([[0.], [1.]])
+print(u.dot(v))
+
+u = Vector([[1.], [1.]])
+v = Vector([[1.], [1.]])
+print(u.dot(v))
+
+u = Vector([[4.], [2.]])
+v = Vector([[2.], [1.]])
+print(u.dot(v))
+
+print("=======eval===========")
