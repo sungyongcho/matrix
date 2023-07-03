e3b4e8119a442cff738a2918ffbb0c3029018d75
Sungyong Cho <here@sungyongcho.com>
2023-07-03 04:54:52 +0200

    fix: ex08 eval done

--- srcs/ex08.py
+++ srcs/ex08.py
@@ -15,3 +15,22 @@ u = Matrix([[-2., -8., 4.], [1., -23., 4.], [0., 6., 4.]])
 
 # -21.0
 print(u.trace())
+
+print("=======eval===========")
+
+u = Matrix([[0., 0.], [0., 0.]])
+print(u.trace())
+
+u = Matrix([[1., 0.], [0., 1.]])
+print(u.trace())
+
+u = Matrix([[1., 2.], [3., 4.]])
+print(u.trace())
+
+u = Matrix([[8., -7.], [4., 2.]])
+print(u.trace())
+
+u = Matrix([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])
+print(u.trace())
+
+print("=======eval===========")
