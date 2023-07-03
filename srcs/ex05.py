b04b16df17bd8a3b68dba72d612481292379457a
Sungyong Cho <here@sungyongcho.com>
2023-07-03 04:46:55 +0200

    fix: ex05 eval done

--- srcs/ex05.py
+++ srcs/ex05.py
@@ -29,3 +29,33 @@ v = Vector([[4.], [5.], [6.]])
 
 # 0.974631846
 print(angle_cos(u, v, 0))
+
+print("=======eval===========")
+
+u = Vector([[1.], [0.]])
+v = Vector([[0.], [1.]])
+
+print(angle_cos(u, v))
+
+u = Vector([[8.], [7.]])
+v = Vector([[3.], [2.]])
+
+# decimal place rounding disabled
+print(angle_cos(u, v, 0))
+
+u = Vector([[1.], [1.]])
+v = Vector([[1.], [1.]])
+
+print(angle_cos(u, v))
+
+u = Vector([[4.], [2.]])
+v = Vector([[1.], [1.]])
+
+print(angle_cos(u, v, 0))
+
+u = Vector([[-7.], [3.]])
+v = Vector([[6.], [4.]])
+
+print(angle_cos(u, v, 0))
+
+print("=======eval===========")
