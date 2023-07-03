c8506bbe18c337af4a6707d5919d910bbb1794d0
Sungyong Cho <here@sungyongcho.com>
2023-07-03 04:44:04 +0200

    fix: ex04 eval done

--- srcs/ex04.py
+++ srcs/ex04.py
@@ -10,3 +10,28 @@ print(u.norm_1(), u.norm(), u.norm_inf())
 u = Vector([[-1.], [-2.]])
 # 6.0, 3.74165738, 3.0
 print(u.norm_1(), u.norm(), u.norm_inf())
+
+print("=======eval===========")
+
+u = Vector([[0.]])
+print(u.norm(), u.norm_1())
+
+u = Vector([[1.]])
+print(u.norm(), u.norm_1())
+
+u = Vector([[0.], [0.]])
+print(u.norm(), u.norm_1())
+
+u = Vector([[1.], [0.]])
+print(u.norm(), u.norm_1())
+
+u = Vector([[2.], [1.]])
+print(u.norm(), u.norm_1())
+
+u = Vector([[4.], [2.]])
+print(u.norm(), u.norm_1())
+
+u = Vector([[-4.], [-2.]])
+print(u.norm(), u.norm_1())
+
+print("=======eval===========")
