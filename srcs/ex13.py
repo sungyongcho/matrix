b7791aed84192ac6a1396f330d2b3fe0bdade66c
Sungyong Cho <here@sungyongcho.com>
2023-07-03 05:17:39 +0200

    fix: ex13 eval done

--- srcs/ex13.py
+++ srcs/ex13.py
@@ -20,3 +20,33 @@ u = Matrix([[8., 5., -2.],
 
 # # 3
 print(u.rank())
+
+print("=======eval===========")
+
+u = Matrix([[0., 0.], [0., 0.]])
+print(u.rank())
+
+u = Matrix([[1., 0.], [0., 1.]])
+print(u.rank())
+
+u = Matrix([[2., 0.], [0., 2.]])
+print(u.rank())
+
+u = Matrix([[1., 1.], [1., 1.]])
+print(u.rank())
+
+u = Matrix([[0., 1.], [1., 0.]])
+print(u.rank())
+
+u = Matrix([[1., 2.], [3., 4.]])
+print(u.rank())
+
+u = Matrix([[-7., 5.], [4., 6.]])
+print(u.rank())
+
+u = Matrix([[1., 0., 0.],
+            [0., 1., 0.],
+            [0., 0., 1.]])
+print(u.rank())
+
+print("=======eval===========")
